b9a2a5ee99ff6cb1f2fb9877bd6ff590621660c8
ecarlosfonseca <ecarlosfonseca@gmail.com>
2020-06-25 17:12:52 +0100

    correction

--- SeparateTheNumbers.py
+++ SeparateTheNumbers.py
@@ -1,32 +1,47 @@
 def separateNumbers(s):
 
-    for i in range(1, len(s)//2):
-        strings = []
+    p = False
+    for i in range(1, len(s)//2+1):
         sub_string = []
         j = 0
         first = True
+        l = 0
+
         while j + i <= len(s):
             if not first:
-                if sub_string[0] == '9' * i:
+                if sub_string[-1] == '9' * i or len(sub_string[-1]) > i:
                     sub_string.append(s[j:j + i + 1])
+                    l += len((s[j:j + i+1]))
                     j += i + 1
                 else:
                     sub_string.append(s[j:j + i])
+                    l += len((s[j:j + i]))
                     j += i
+
             else:
                 sub_string.append(s[j:j + i])
+                l += len((s[j:j + i]))
                 j += i
                 first = False
 
-        for j in range(len(sub_string)-1, 0, -1):
-            if int(sub_string[j]) - int(sub_string[j-1]) != 1:
-                break
-            print('YES' + ' ' + sub_string[0])
-            break
+        if sum(map(int, sub_string)) == len(sub_string) * int(sub_string[0]) + sum(list(range(len(sub_string)))):
+            if len(sub_string[0]) > 1 and int(sub_string[0][0]) == 0:
+                pass
+            elif int(sub_string[1]) - int(sub_string[0]) == 1 and len(s) % len(sub_string) == 0 and l == len(s):
+                p = True
+                print('YES' + ' ' + sub_string[0])
+
+    if not p:
+        print('NO')
 
 
 if __name__ == '__main__':
     s0 = '99910001001'
     s1 = '7891011'
     s2 = '9899100'
-    separateNumbers(s1)
+    s3 = '999100010001'
+    s4 = '99100'
+    s5 = '010203'
+    s6 = '4294967295429496729642949672'
+    separateNumbers(s6)
+
